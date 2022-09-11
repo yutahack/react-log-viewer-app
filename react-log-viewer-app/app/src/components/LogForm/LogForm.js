e6bdaddd3228d6949cd4b2ee0093af283b4a0cf5
yutahack <yutahack@gmail.com>
2022-09-11 22:04:41 +0900

    LogForm 주석 정리 및 로직 변경

--- react-log-viewer-app/app/src/components/LogForm/LogForm.js
+++ react-log-viewer-app/app/src/components/LogForm/LogForm.js
@@ -30,78 +30,25 @@ const LogForm = (props) => {
 
         console.log("### LogForm: Clicked and got conditions!", props.filterConditionGetter);
 
-        // // 조건 분류하기
-        // var tmp1 = [];
-        // if (undefined !== props.filterConditionGetter.logLevelConditions) {
-        //     var llc = props.filterConditionGetter.logLevelConditions;
-
-        //     for (let i = 0; i < llc.length; i++) {
-        //         tmp1.push(llc[i]);
-        //     }
-        // }
-        // setLogLevelConditions(tmp1);
-        // var tmp2 = [];
-        // if (undefined !== props.filterConditionGetter.wordConditions) {
-        //     var wc = props.filterConditionGetter.wordConditions;
-        //     for (let i = 0; i < wc.length; i++) {
-        //         tmp2.push(wc[i]);
-        //     }
-        // }
-        // setWordConditions(tmp2);
-
-        // 필터링, 조건이 undefined의 경우 수행하지 않음.
-        // if (undefined === logLevelConditions || undefined === wordConditions) {
-        // var filtered = Filter({ logLevel: logLevelConditions, word: wordConditions });
-
-        // var filtered = Filter(props.filterConditionGetter);
-        // setfilteredLogs(filtered);
-
-        // filter(props.filterConditionGetter);
-
-        console.log("SET!");
         var t = filter(props.filterConditionGetter);
         setfilteredLogs(t);
-        // } else {
-        //     console.log("### LogForm: Some conditions undefined!");
-        // }
     }, [props.searchEventGetter]);
 
-    // useEffect(() => {
-    //     console.log("SOT!", conditions);
-    //     var filtered = filter(conditions);
-    //     // setfilteredLogs(filtered);
-    // }, [conditions]);
-
-    // useEffect(() => {
-    //     // 이중 실행 방지
-    //     console.log("LLC", logLevelConditions.length);
-    //     console.log("WC", wordConditions.length);
-    //     // 필터링, 조건이 undefined의 경우 수행하지 않음.
-    //     if (logLevelConditions.length > 0 && wordConditions.length > 0) {
-    //         console.log("### LogForm: Filtering...");
-    // var filtered = Filter({ logLevel: logLevelConditions, word: wordConditions });
-    // setfilteredLogs(filtered);
-    //     } else {
-    //         console.log("### LogForm: Some conditions undefined!");
-    //     }
-    // }, [logLevelConditions, wordConditions]);
-
-    // MainFrame으로부터 로그 전체를 넘겨받았을 때 실행
     useEffect(() => {
         if (props.logJsonGetter) {
             setlogJson(props.logJsonGetter);
         }
     }, [props.logJsonGetter]);
 
-    // 전체 로그를 넘겨받고 필터 로직을 실행
     useEffect(() => {
-        // var filtered = Filter({ logLevel: logLevelConditions, word: wordConditions });
-        // setfilteredLogs(filtered);
-    }, [logJson]);
+        console.log("DLD", displayLogData);
+    }, [displayLogData]);
+
     // 필터가 완료되면 화면에 뿌려주기
     useEffect(() => {
+        console.log("fetch");
         fetchMoreData();
-    }, [filteredLogs]);
+    }, [filteredLogs, props.searchEventGetter]);
 
     // 필터링 로직
     const filter = (input) => {
@@ -111,33 +58,8 @@ const LogForm = (props) => {
         // var filtered = "";
         var filtered = logJson;
 
-        // input.logLevelConditions.map((v, i) => {
-        //     console.log(v, i);
-        // });
-        // for (let i = 0; i < input.logLevelConditions.length; i++) {
-        //     console.log(i);
-        // }
-
-        console.log("1", 0 === input.logLevelConditions.length);
-        console.log("2", undefined === input.logLevelConditions[0]);
         if (0 === input.logLevelConditions.length && undefined === input.logLevelConditions[0]) {
-            console.log("T");
         } else {
-            // filtered = filtered.filter((e) => {
-            //     var t = [];
-            //     for (let i = 0; i < input.logLevelConditions.length; i++) {
-            //         if ("" === input.logLevelConditions[i]) {
-            //             continue;
-            //         }
-            //         if (e.logLevel === input.logLevelConditions[i]) {
-            //             console.log("Matched!");
-            //             // t.push(e);
-            //         }
-            //     }
-            //     return t;
-            // });
-            console.log("F", filtered.length);
-            console.log("L", input.logLevelConditions.length);
             var t = [];
             for (let i = 0; i < filtered.length; i++) {
                 for (let j = 0; j < input.logLevelConditions.length; j++) {
@@ -147,51 +69,14 @@ const LogForm = (props) => {
                     }
                 }
             }
-            console.log("t", t);
             filtered = t;
         }
-        // if (input.logLevelConditions.length === 1 && undefined ===  input.logLevelConditions[0]) {
-        //     filtered = filtered.filter((e) => {
-        //         var t = [];
-        //         for (let i = 0; i < input.logLevelConditions.length; i++) {
-        //             if ("" === input.logLevelConditions[i]) {
-        //                 continue;
-        //             }
-        //             if (e.logLevel === input.logLevelConditions[i]) {
-        //                 console.log("Matched!");
-        //                 t.push(e);
-        //             }
-        //         }
-        //         return t;
-        //     });
-        // }
 
         console.log("Loglevel filtered: ", filtered);
         console.log("Input wordConditions logs: ", input.wordConditions.length);
-        //     if (input.wordConditions.length > 0) {
-        //         filtered = filtered.filter((e) => {
-        //             var t = "";
-        //             for (let i = 0; i < input.wordConditions.length; i++) {
-        //                 if ("" === input.wordConditions[i]) {
-        //                     continue;
-        //                 }
-        //                 t = e.message.toUpperCase().includes(input.wordConditions[i].toUpperCase());
-        //             }
-        //             return t;
-        //         });
-        //     } else {
-        //         filtered = filtered;
-        //     }
-        //     console.log("### Loglevel + Message Filtered!", filtered);
-        //     return filtered;
-        // };
 
         if (0 === input.wordConditions.length && undefined === input.wordConditions[0]) {
-            console.log("2T");
         } else {
-            console.log("2F", filtered.length);
-            console.log("2L", input.wordConditions.length);
-
             if (input.wordConditions.length > 0) {
                 filtered = filtered.filter((e) => {
                     var t2 = [];
@@ -211,36 +96,6 @@ const LogForm = (props) => {
         return filtered;
     };
 
-    // // 필터링 로직
-    // const Filter = (input) => {
-    //     // Log level filter
-    //     console.log("Before filtered: ", logJson);
-    //     var filtered = "";
-    //     if ("" !== input.logLevel) {
-    //         filtered = logJson.filter((e) => e.logLevel === input.logLevel);
-    //     } else {
-    //         filtered = logJson;
-    //     }
-    //     console.log("Loglevel filtered: ", filtered);
-    //     console.log("Input message logs: ", input.message.length);
-    //     if (input.message.length > 0) {
-    //         filtered = filtered.filter((e) => {
-    //             var t = "";
-    //             for (let i = 0; i < input.message.length; i++) {
-    //                 if ("" === input.message[i]) {
-    //                     continue;
-    //                 }
-    //                 t = e.message.toUpperCase().includes(input.message[i].toUpperCase());
-    //             }
-    //             return t;
-    //         });
-    //     } else {
-    //         filtered = filtered;
-    //     }
-    //     console.log("Lovelvel + Message Filtered!", filtered);
-    //     return filtered;
-    // };
-
     // debug
     useEffect(() => {
         console.log("### Page: ", page);
@@ -257,6 +112,7 @@ const LogForm = (props) => {
                 setDisplayLogData([]);
                 var res = filteredLogs.slice(page * numOfSearchData, page * numOfSearchData + numOfSearchData);
                 setDisplayLogData(displayLogData.concat(res));
+                // setDisplayLogData([...displayLogData, res]);
 
                 if (res.length > 0) {
                     setPage((prev) => prev + 1);
